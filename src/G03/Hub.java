ee7f62a593bcd28d411e057a37b389be889d53fe
MiguelAngelBallesteros <miguelangel.ballesteros@alu.uclm.es>
2023-03-16 19:48:44 +0100

    Constructor Hub

--- src/G03/Hub.java
+++ src/G03/Hub.java
@@ -1,25 +1,17 @@
 package G03;
+
 import G03.Contenedores;
+
 public class Hub {
-    private Contenedores[][] c;
+    private Contenedores[][] c=new Contenedores[10][12];
 
     public Hub() {
-
-    }
-    public String mostrar_datos(int id){
-        String datos=new String();
         for(int i=0;i<10;i++){
             for(int j=0;j<12;j++){
-                if(c[i][j].getIdentificador()==id){
-                    datos=c[i][j].toString();
-                    return datos;
-                }
+                c[i][j].setIdentificador(0);
             }
         }
-        datos="Ese contenedor no esta en el hub";
-        return datos;
     }
-
 
     public String toString() {
         String s = " ";
