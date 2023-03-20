d280ce426196f9e1fc69e99b56dd1ba0632059f4
MiguelAngelBallesteros <miguelangel.ballesteros@alu.uclm.es>
2023-03-20 17:44:48 +0100

    Correcion metodo apilar contenedor

--- src/G03/Hub.java
+++ src/G03/Hub.java
@@ -67,20 +67,20 @@ public class Hub {
     }
     public void ApilarContenedor(Contenedores contenedores) {
         if (contenedores.getPrioridad() == 1) {
-            for (int i = 10; i > 0; i--) {
+            for (int i = 9; i >= 0; i--) {
                 if (c[i][0].getIdentificador() == 0) {
                     c[i][0] = contenedores;
                 }
             }
         } else if (contenedores.getPrioridad() == 2) {
-            for (int i = 10; i > 0; i--) {
+            for (int i = 9; i >= 0; i--) {
                 if (c[i][1].getIdentificador() == 0) {
                     c[i][1] = contenedores;
                 }
             }
         } else if (contenedores.getPrioridad() == 3) {
-            for (int i = 10; i > 0; i--) {
-                for (int j = 12; j > 2; j--) {
+            for (int i = 9; i >= 0; i--) {
+                for (int j = 11; j > 1; j--) {
                     if (c[i][j].getIdentificador() == 0) {
                         c[i][j] = contenedores;
                         break;
